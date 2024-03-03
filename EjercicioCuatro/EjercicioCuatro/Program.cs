6c89876a59f59848cd86b438ad37b9fd02d04fa4
Hurt9901 <brayanroco99@gmail.com>
2024-03-03 09:43:07 -0500

    Primer commit. Creacion del proyecto.

---
+++ EjercicioCuatro/EjercicioCuatro/Program.cs
@@ -0,0 +1,10 @@
+﻿/*Realizar  un  programa  para  una  editorial  que  publica  libros  y  periódicos,
+ * en  el  programa  se  deben  manejar  las  siguientes  variables:
+ * Número  de  hojas,  valor  hoja,  hojas  a  color,  titulo,  tipo  de  portada,
+ * nombre autor, nombre periódico y fecha. Emplear una superclase publicaciones,
+ * dos subclases libro y periódico La clase principal o editorial debe crear objetos
+ * tanto libro como periódico y calcular el costo que  se  obtiene  de:
+ * el  valor  de  la  hoja,  el  tipo  de  portada  (sus  valores  son
+ * lujo  =  10.000$,  normal  =  5.000$ y económica = 3.000$)
+ * y si es a color cada hoja vales 200$ más que a blanco y negro 
+ */
